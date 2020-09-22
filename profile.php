e0b9c02e2e5d56124182ea50c05eb1d25be0a541
fez7 <fez7@users.noreply.github.com>
2020-09-23 04:39:09 +0800

    more styling

--- profile.php
+++ profile.php
@@ -46,7 +46,7 @@
             </nav>
         </div>
 
-        <div class="tableContainer" style="height:150vh;">
+        <div class="tableContainer" style="height:100vh;">
         <?php
             echo "<table class='userTable' style='height:600px;'>";
             echo "<tr>";
